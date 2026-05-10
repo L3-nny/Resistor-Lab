795345c10cd484870b4a3b3a67964f92168d7582
kiplagatkate-glitch <kiplagat.kate@students.jkuat.ac.ke>
2026-05-10 13:51:18 +0300

    Task 7 AC Impedance-Friend function
    Use a fixed stray capacitance of 1pF. Access r.resistance. Compute Xc then return magnitude of complex impedance |Z|.
    
    fomula used
    
    |Z| = sqrt(R² + Xc²) Xc = 1/(2πfC)
    
    output
    
    Circuit initialized with R1 and R2
    Impedance of R1: 3.1831e+09 Ohms At 50 Hz
    Impedance of R2: 3.1831e+09 Ohms At 50 Hz

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -17,20 +18,45 @@ private:
     string label;
     double resistance; // in Ohms
     double tolerance;  // e.g., 0.05 for 5%
+    double freqHz;      // in Hz (for future use, currently not utilized)
 
 public:
-    Resistor(string l, double r, double t) : label(l), resistance(r), tolerance(t) {}
-
-    // Getters - These make the current code "clunky"
-    double getR() const { return resistance; }
-    double getT() const { return tolerance; }
+    Resistor(string l, double r, double t) : label(l), resistance(r), tolerance(t){}    // Getters - These make the current code "clunky"
     string getL() const { return label; }
+
+friend double acImpedance(const Resistor& r, double freqHz); // Friend function declaration
 };
 
 int main() {
     Resistor r1("R1", 1000, 0.05); // 1k Ohm
     Resistor r2("R2", 2200, 0.10); // 2.2k Ohm
 
+    double freqHz = 50;
+    double Z1 = acImpedance(r1, freqHz); // Calculate impedance
+    double Z2 = acImpedance(r2, freqHz); // Calculate impedance
+
     cout << "Circuit initialized with " << r1.getL() << " and " << r2.getL() << endl;
+
+    cout << "Impedance of " << r1.getL() << ": " << Z1 << " Ohms" <<" At "<< freqHz << " Hz" << endl;
+    cout << "Impedance of " << r2.getL() << ": " << Z2 << " Ohms" << " At "<< freqHz << " Hz" << endl;
+
     return 0;
+}
+
+ //Friend function definition
+double acImpedance(const Resistor& r, double freqHz) {
+  const double omega = 2 * M_PI * freqHz; 
+        double X_C = 0 ; // Capacitive reactance
+        double C = 1e-12; //12pF
+
+    // Prevent division by zero if capacitance is 0
+    if (freqHz > 0) {
+         X_C = 1 / (omega * C); 
+    } else {
+        
+    }
+
+    double Z = sqrt(pow(r.resistance, 2) + pow(( X_C), 2)); 
+    return Z;
+
 }
